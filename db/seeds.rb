da9cf0f733beea41fd53f3670b3fcf4dca3a6b72
nicola bouffard <nicola.bouffard855@gmail.com>
2023-05-18 16:13:55 +0200

    add seed restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database....'
+
+Restaurant.destroy_all
+
+puts 'Creating new data'
+
+10.times do
+  Restaurant.create!(name: Faker::Restaurant.name, address: Faker::Address.street_address, category: Restaurant::CATEGORIES.sample)
+end
+
+puts 'Finished!'
