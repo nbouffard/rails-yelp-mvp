e84d2c902ee3ed956a11cb703e9d0b17b29d85e7
nicola bouffard <nicola.bouffard855@gmail.com>
2023-05-19 21:52:00 +0200

    add create review, add links to create review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
   validates :rating, :content, presence: true
-  # validates :rating, numericality: { only_integer: true }
-  validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5, only_integer: true }
+  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }
 end
